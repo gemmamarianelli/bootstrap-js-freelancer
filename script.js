2ffc1c5208c6171233a19b5fdba4d839b83417be
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-11-18 12:14:24 +0100

    primo push

---
+++ script.js
@@ -0,0 +1,21 @@
+//importazione elementi dal html
+
+let element = document.getElementById("submit");
+let ore = document.getElementById("inputHour").value;
+let lavoro = document.getElementById("inputWork").value;
+let sconto = document.getElementById("inputDiscount").value;
+
+
+let sconti = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+//-------------------------------------FUNZIONI----------------------------------------------
+function sconti(){
+    
+}
+function codiciSconto(sconto){
+    if(sconti != sconto){
+
+    }else{
+
+    }
+}
